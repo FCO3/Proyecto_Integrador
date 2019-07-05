cf408de8bc22ab577fe5380ab9c47f417df90d86
hw17 <hectorwarneros@gmail.com>
2019-07-05 16:32:00 -0500

    This is the very begining of the project...

---
+++ Debug/synergy/board/s7g2_sk/bsp_init.d
@@ -0,0 +1,133 @@
+synergy/board/s7g2_sk/bsp_init.o: ../synergy/board/s7g2_sk/bsp_init.c \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp/bsp_api.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\inc\ssp_common_api.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\inc\ssp_version.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\board\s7g2_sk\bsp.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\board\s7g2_sk\bsp_init.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\board\s7g2_sk\bsp_leds.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\board\s7g2_sk\bsp_qspi.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\board\s7g2_sk\bsp_ethernet_phy.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\inc\ssp_common_api.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_locking.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_irq.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\driver\api/r_ioport_api.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\inc\ssp_features.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\inc\ssp_common_api.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\driver\instances/r_fmi.h \
+ C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\driver\api/r_fmi_api.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_delay.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
+ c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp/bsp_api.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\inc\ssp_common_api.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\inc\ssp_version.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\board\s7g2_sk\bsp.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\board\s7g2_sk\bsp_init.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\board\s7g2_sk\bsp_leds.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\board\s7g2_sk\bsp_qspi.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\board\s7g2_sk\bsp_ethernet_phy.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\inc\ssp_common_api.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_locking.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_irq.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\driver\api/r_ioport_api.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\inc\ssp_features.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\inc\ssp_common_api.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\driver\instances/r_fmi.h:
+
+C:\Users\Hector\ Guarneros\e2_studio\workspace\GUIApp_Hector\synergy\ssp\inc\driver\api/r_fmi_api.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_delay.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_feature.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h:
+
+c:\users\hector\ guarneros\e2_studio\workspace\guiapp_hector\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
